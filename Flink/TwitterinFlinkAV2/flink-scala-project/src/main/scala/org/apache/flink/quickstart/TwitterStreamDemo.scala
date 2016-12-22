8669e7319960a87cc0d43a250f065723ce979718
Andres Vivanco Villamar <andresvivancov@gmail.com>
2016-12-22 02:13:52 +0100

    Its running :)
    :)

---
+++ Flink/TwitterinFlinkAV2/flink-scala-project/src/main/scala/org/apache/flink/quickstart/TwitterStreamDemo.scala
@@ -0,0 +1,196 @@
+package org.apache.flink.quickstart
+
+/**
+  * Created by andresviv on 11.12.16.
+  */
+import java.util.Date
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.evictors.CountEvictor
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.triggers.CountTrigger
+import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
+import org.apache.flink.api.common.functions.MapFunction
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.table.expressions.Count
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.util.Collector
+
+/**
+  * Describes a Twitter location as Longitude/Latitude
+  */
+case class TwitterLocation(var lat: Double, var lon: Double) {
+  def this(){
+    this(0.0,0.0)
+  }
+}
+
+/**
+  * Describes a single tweet
+  */
+case class Tweet(userName: String, content: String, createdAt: Date, location: TwitterLocation, language: String)
+{
+  def this(){
+    this("__NO_USER__","__NO_CONTENT__",new Date(), new TwitterLocation(0.0,0.0),"")
+  }
+}
+
+/**
+  * This class describes the source of incoming twitter feeds and is used by the DataStream API
+  * It implements the SourceFunction class
+  */
+class TwitterStreamGenerator(filterTerms: String*) extends SourceFunction[Tweet]{
+  var running = true
+
+  override def cancel(): Unit = {
+    running = false
+  }
+
+  /**
+    * This method is being called by DataStream API to kick-off the stream-production
+    */
+  override def run(ctx: SourceContext[Tweet]): Unit = {
+    val cb = new ConfigurationBuilder
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey("X0k4zycdWOV4UT0QuU7gLUdiU")
+      .setOAuthConsumerSecret("nq42UJ75jVhYTHOsmpRfHFdanXOj9qreAvP5OzaWw6Gt38MGnU")
+      .setOAuthAccessToken("1206314257-gPBrxJk1FoeHovE5I0CO0QKhDqIry5pXkenRlUW")
+      .setOAuthAccessTokenSecret("UM262OPTvNQ4INwleQh4jRVV1ZqLWAKMgR86a8GTVJDcM")
+
+    val stream: TwitterStream = new TwitterStreamFactory(cb.build()).getInstance()
+
+    stream.addListener(new SimpleStreamListener(ctx))
+    val query = new FilterQuery(0, null, filterTerms.toArray)
+    stream.filter(query)
+    while(running){}
+  }
+}
+
+/**
+  * This class represents the Listener needed by Twitter4J
+  * Here we define methods which are being called by the Twitter API (especially the "onStatus" method is very important)
+  * To make the DataStream API aware of our incoming tweets we instantiate SimpleStreamListener by providing it the
+  * SourceContext instance delivered by DataStream API. Inside onStatus method the collect() method of SourceContext
+  * will be called to forward new tweets to it. Therefore the SimpleStreamListener is hard-wired with the TwitterStreamGenerator
+  */
+class SimpleStreamListener(ctx: SourceContext[Tweet]) extends StatusListener() {
+  override def onStatus(status: Status) = {
+    val userName = status.getUser.getName
+    val content = status.getText
+    val createdAt = status.getCreatedAt
+    val geoLoc = status.getGeoLocation
+    val location = geoLoc match {
+      case null => new TwitterLocation(0.0,0.0)
+      case _ => new TwitterLocation(geoLoc.getLatitude, geoLoc.getLongitude)
+    }
+    val language = status.getLang
+    val tweet = Tweet(userName, content, createdAt, location, language)
+    ctx.collect(tweet)
+  }
+
+  override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) = {}
+
+  override def onTrackLimitationNotice(numberOfLimitedStatuses: Int) = {}
+
+  override def onException(ex: Exception) = {
+    ex.printStackTrace
+  }
+
+  def onStallWarning(warning: StallWarning) = {}
+  def onScrubGeo(l: Long, l1: Long) = {}
+}
+
+/**
+  * Our application starts here.
+  * We get the StreamingContext and declare a new Stream-Source by instatiating the TwitterStreamGenerator
+  * which will only forward tweets containing certain #hash-tags
+  */
+/**Origintal
+  *
+  * object TwitterStreamDemo {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val tweets: DataStream[Tweet] = env.addSource(new TwitterStreamGenerator("#machinelearning","#datascience"))
+    tweets.map(tweet => println(s"$tweet.userName : $tweet.content"))
+    env.execute("TwitterStream")
+  }
+
+  *
+  */
+
+//Extract the stock symbols
+
+
+
+
+object TwitterStreamDemo {
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val tweets: DataStream[Tweet] = env.addSource(new TwitterStreamGenerator("#machinelearning","#datascience"))
+
+/**
+    val timedValue = tweets.assignAscendingTimestamps(x => x.userName(1))
+
+    val keyValuePair = timedValue.keyBy(0).timeWindow(Time.milliseconds(3))
+
+    val countStream = keyValuePair.sum(1)//.name("countStream")
+    */
+
+    //  val values = tweets.flatMap(x => x.userName)
+
+    val keyValue = tweets.keyBy(x => x.userName)
+    val tumblingWindow = keyValue.timeWindow(Time.seconds(15))
+    val countWindow = keyValue.countWindow(5)
+
+  println(keyValue)
+
+   // val values = tweets.flatMap(value => value.split("\\s+")).map(value => (value,1))
+
+
+    /*
+
+
+          .keyBy(0)
+          .countWindow(windowSize, slideSize)
+          // group by the tuple field "0" and sum up tuple field "1"
+          .sum(1);
+      */
+    env.execute("TwitterStream")
+  }
+
+
+  /**
+    *
+    * Taxi Rides
+    *
+    *  // find n most popular spots
+    val popularPlaces = rides
+      // remove all rides which are not within NYC
+      .filter { r => GeoUtils.isInNYC(r.startLon, r.startLat) && GeoUtils.isInNYC(r.endLon, r.endLat) }
+      // match ride to grid cell and event type (start or end)
+      .map(new GridCellMatcher)
+      // partition by cell id and event type
+      .keyBy( k => k )
+      // build sliding window
+      .timeWindow(Time.minutes(15), Time.minutes(5))
+      // count events in window
+      .apply{ (key: (Int, Boolean), window, vals, out: Collector[(Int, Long, Boolean, Int)]) =>
+        out.collect( (key._1, window.getEnd, key._2, vals.size) )
+      }
+      // filter by popularity threshold
+      .filter( c => { c._4 >= popThreshold } )
+      // map grid cell to coordinates
+      .map(new GridToCoordinates)
+    *
+    */
+
+
+
+}
