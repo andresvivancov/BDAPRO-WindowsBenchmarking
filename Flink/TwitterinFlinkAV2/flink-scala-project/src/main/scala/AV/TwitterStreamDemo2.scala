98d5a7bf2bce804ef2dfc849fef842dcb632718b
Andres Vivanco Villamar <andresvivancov@gmail.com>
2016-12-22 02:17:51 +0100

    Update TwitterStreamDemo2.scala

--- Flink/TwitterinFlinkAV2/flink-scala-project/src/main/scala/AV/TwitterStreamDemo2.scala
+++ Flink/TwitterinFlinkAV2/flink-scala-project/src/main/scala/AV/TwitterStreamDemo2.scala
@@ -58,10 +58,10 @@ class TwitterStreamGenerator(filterTerms: String*) extends SourceFunction[Tweet]
   override def run(ctx: SourceContext[Tweet]): Unit = {
     val cb = new ConfigurationBuilder
     cb.setDebugEnabled(true)
-      .setOAuthConsumerKey("X0k4zycdWOV4UT0QuU7gLUdiU")
-      .setOAuthConsumerSecret("nq42UJ75jVhYTHOsmpRfHFdanXOj9qreAvP5OzaWw6Gt38MGnU")
-      .setOAuthAccessToken("1206314257-gPBrxJk1FoeHovE5I0CO0QKhDqIry5pXkenRlUW")
-      .setOAuthAccessTokenSecret("UM262OPTvNQ4INwleQh4jRVV1ZqLWAKMgR86a8GTVJDcM")
+      .setOAuthConsumerKey("X0kuU7gLUdiU")
+      .setOAuthConsumerSecret("nq42UJ7Gt38MGnU")
+      .setOAuthAccessToken("1206314257-gPBqIry5pXkenRlUW")
+      .setOAuthAccessTokenSecret("UM262OPTTVJDcM")
 
     val stream: TwitterStream = new TwitterStreamFactory(cb.build()).getInstance()
 
